a0758979bc94c3381e6d152d5823d678a836e5b3
Fabien Kost <closi.qode@gmail.com>
2026-03-29 22:54:10 +0200

    fix: ajouter migration de merge pour résoudre les branches parallèles upstream/fork

---
+++ bots/migrations/0082_merge_upstream.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bots', '0079_remove_botevent_valid_event_type_event_sub_type_combinations_and_more'),
+        ('bots', '0081_aisummarysettings_reasoning_effort'),
+    ]
+
+    operations = [
+    ]
